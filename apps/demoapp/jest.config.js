c150121fffa5ab5842909bc922f47b663b66e56c
Michael Burger <michael.burger@orma-solutions.it>
2021-04-20 10:00:35 +0200

    chore(dependencies): update @nrwl/workspace@10.0.0

--- apps/demoapp/jest.config.js
+++ apps/demoapp/jest.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   name: 'demoapp',
   preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/apps/demoapp/'
+  coverageDirectory: '../../coverage/apps/demoapp/',
+  setupFilesAfterEnv: ['<rootDir>src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      astTransformers: [
+        'jest-preset-angular/build/InlineFilesTransformer',
+        'jest-preset-angular/build/StripStylesTransformer',
+      ],
+    },
+  },
 };
--- libs/ngx-lazy-el/jest.config.js
+++ libs/ngx-lazy-el/jest.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   name: 'ngx-lazy-el',
   preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/libs/ngx-lazy-el'
+  coverageDirectory: '../../coverage/libs/ngx-lazy-el',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      astTransformers: [
+        'jest-preset-angular/build/InlineFilesTransformer',
+        'jest-preset-angular/build/StripStylesTransformer',
+      ],
+    },
+  },
 };
