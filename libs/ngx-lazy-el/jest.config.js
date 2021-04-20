73b6ad7221dc689e5f3cde176c37af61041be7dd
Michael Burger <michael.burger@orma-solutions.it>
2021-04-20 10:26:04 +0200

    chore(dependencies): update @nrwl/workspace@10.4.15

--- libs/ngx-lazy-el/jest.config.js
+++ libs/ngx-lazy-el/jest.config.js
@@ -1,16 +1,18 @@
 module.exports = {
-  name: 'ngx-lazy-el',
-  preset: '../../jest.config.js',
+  preset: '../../jest.preset.js',
   coverageDirectory: '../../coverage/libs/ngx-lazy-el',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
     'ts-jest': {
       tsConfig: '<rootDir>/tsconfig.spec.json',
       stringifyContentPathRegex: '\\.(html|svg)$',
-      astTransformers: [
-        'jest-preset-angular/build/InlineFilesTransformer',
-        'jest-preset-angular/build/StripStylesTransformer',
-      ],
+      astTransformers: {
+        before: [
+          'jest-preset-angular/build/InlineFilesTransformer',
+          'jest-preset-angular/build/StripStylesTransformer',
+        ],
+      },
     },
   },
+  displayName: 'ngx-lazy-el',
 };
